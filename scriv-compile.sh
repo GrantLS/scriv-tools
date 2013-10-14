fa1b136fe71b05af697c5aa95b69ac2d3768c508
GrantLS <glsimpso@gmail.com>
2013-10-14 13:07:14 -0400

    correcting path references

--- scriv-compile.sh
+++ scriv-compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #expect $1 to be working directory and $2 to be file name (w/out .scriv)
-./scriv-get-xml.pl "$1/$2.scriv" | ./scriv-get-for-compile.pl | \
-./scriv-get-rtf-for-conversion.pl "$1/$2.scriv" > "$1/$2.tex"
+scriv-get-xml.pl "$1/$2.scriv" | scriv-get-for-compile.pl | \
+scriv-get-rtf-for-conversion.pl "$1/$2.scriv" > "$1/$2.tex"
 
 /usr/texbin/pdflatex -interaction=batchmode "$1/$2.tex"
 /usr/texbin/pdflatex -interaction=batchmode "$1/$2.tex"
